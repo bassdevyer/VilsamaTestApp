2a93b7829271c4d64d6f3f9657b272cb795d40e6
miguetv17 <miguetv17@gmail.com>
2016-03-04 16:00:19 -0500

    Initial commit

---
+++ app/src/main/java/com/tie_vilsama/vilsamatestapp/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.tie_vilsama.vilsamatestapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Activity que gestiona autenticacion de usuario
+ * @author mtorres
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/test/java/com/tie_vilsama/vilsamatestapp/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.tie_vilsama.vilsamatestapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
